2a9869b5459511d8993df63d44091eb381b7aa05
Amaraj Judge <amaraj@umich.edu>
2015-07-08 22:17:54 -0400

    added the admin user via ActiveAdmin as well as the models for the Survey and the join table, SurveyQuestion

---
+++ app/models/survey.rb
@@ -0,0 +1,11 @@
+class Survey < ActiveRecord::Base
+end
+
+# == Schema Information
+#
+# Table name: surveys
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
