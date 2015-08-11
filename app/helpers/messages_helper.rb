629ef6df2344d66e7980f100334ff6bccc700d11
Amaraj Judge <amaraj@umich.edu>
2015-08-11 15:37:04 -0400

    helper function in message fixed

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,11 +1,11 @@
 module MessagesHelper
 
-  # def recipient_options
-  #   s = ''
-  #   Mentee.all.each do |mentee|
-  #     s << "<option value='#{mentee.id}'>#{mentee.name}</option>"
-  #   end
-  #   s.html_safe
-  # end
+  def recipient_options
+    s = ''
+    Mentee.all.each do |mentee|
+      s << "<option value='#{mentee.id}'>#{mentee.name}</option>"
+    end
+    s.html_safe
+  end
 
 end
